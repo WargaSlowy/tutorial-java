6a54f6554ba566cb65ee7c1ccdefe641b96b6403
slowy07 <slowy.arfy@proton.me>
2026-09-01 16:46:42 +0700

    chore: komentar kode
    just greening my github heheh
    
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

---
+++ KomentarKode.java
@@ -0,0 +1,23 @@
+public class KomentarKode {
+  public static void main(String[] args) {
+    int umur = 20; // 90
+    System.out.println(umur); // tampilkan
+
+    /* ini adalah multi line comment */
+    String nama = "arfy";
+    // tampilkan informasi
+    System.out.println(nama);
+
+    /**
+     * ini adalah variabel untuk menampung
+     * informasi data penduduk setempat
+     * */
+    int jumlahPenduduk = 200_000;
+    // tampilkan informasi
+    System.out.println(jumlahPenduduk);
+
+    // format id ngikut dari sistem lama
+    // perusahaan
+    String idKostumer = "ID-2030-0087";
+  }
+}
